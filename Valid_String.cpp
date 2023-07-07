91754b29121f67beb062a9c0848c3cf6390d81f1
Brilliant-Developer1 <email2obail@gmail.com>
2023-07-07 17:19:26 +0600

    first commit

---
+++ Valid_String.cpp
@@ -0,0 +1,99 @@
+// #include<bits/stdc++.h>
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <complex>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <cwchar>
+#include <cwctype>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <random>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <strstream>
+#include <tuple>
+#include <typeinfo>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+// #include<bits/stdc++.h>
+using namespace std;
+
+bool isValid(string s)
+{
+    stack<char> st;
+    for (char c : s)
+    {
+        if (!st.empty() && ((c == 'A' && st.top() == 'B') || (c == 'B' && st.top() == 'A')))
+        {
+            st.pop();
+        }
+        else
+        {
+            st.push(c);
+        }
+    }
+    return st.empty();
+}
+
+int main()
+{
+    int input;
+    cin >> input;
+    cin.ignore();
+
+    for (int i = 0; i < input; i++)
+    {
+        string x;
+        getline(cin, x);
+
+        if (isValid(x))
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+
+    return 0;
+}
